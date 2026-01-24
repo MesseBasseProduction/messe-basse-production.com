0df112def54a728814670cb4b976bfef9d9b76a2
ArthurBeaulieu <arthur@beaulieu.email>
2026-01-24 15:10:09 +0100

    feat: add bonus to book

--- app/front/js/subpages/BookPage.js
+++ app/front/js/subpages/BookPage.js
@@ -115,7 +115,62 @@ class BookPage extends AbstractMBP {
   _handleEvents() {
     return new Promise(resolve => {
       this._dom.querySelector('#back-button').addEventListener('click', this._loadPreviousPage.bind(this));
+
+      if (this._isReadingPage === true) {
+        const imgContainers = this._dom.querySelectorAll('.img-container');
+        for (let i = 0; i < imgContainers.length; ++i) {
+          imgContainers[i].children[1].addEventListener('click', () => {
+            const outputImage = document.createElement('A');
+            outputImage.href = `/assets/img/book/MBPPB001/fullres/${imgContainers[i].dataset.file}`;
+            outputImage.download = `${imgContainers[i].dataset.name}`;
+            outputImage.click();
+          });
+
+          imgContainers[i].children[2].addEventListener('click', this._openImgModal.bind(this, {
+            image: `/assets/img/book/MBPPB001/fullres/${imgContainers[i].dataset.file}`,
+            name: imgContainers[i].dataset.name
+          }));
+        }
+      }
       resolve();
+    });
+  }
+
+
+  _openImgModal(photo) {
+    Utils.fetchPage('/modal/photo.html').then(dom => {
+      const modal = document.createElement('DIV');
+      modal.classList.add('modal');
+      modal.classList.add('photo');
+      modal.id = 'single-photo-modal';
+      modal.appendChild(dom);
+      
+      const img = modal.getElementsByTagName('IMG')[0];
+      img.src = photo.image;
+      img.style.boxShadow = '0 0 0 rgba(420, 420, 420, 0)';
+
+      document.getElementById('overlay').appendChild(modal);
+      // Modal opening/closing animation
+      const closeModal = e => {
+        if (['overlay', 'single-photo-modal', 'close-modal'].indexOf(e.target.id) === -1) {
+          return;
+        }
+  
+        document.getElementById('overlay').style.opacity = 0;
+        setTimeout(() => {
+          document.getElementById('overlay').style.display = 'none';
+          document.getElementById('overlay').innerHTML = '';
+        }, 300);
+      };
+
+      modal.removeChild(modal.getElementsByTagName('P')[1]); // First P is close modal
+      document.getElementById('overlay').style.display = 'flex';
+      setTimeout(() => document.getElementById('overlay').style.opacity = 1, 100);
+      setTimeout(() => {
+        modal.style.opacity = 1;
+        document.getElementById('overlay').addEventListener('click', closeModal);
+        document.getElementById('close-modal').addEventListener('click', closeModal);
+      }, 200);
     });
   }
 
